4b944f8ac324a92d662db4673054bc6b923ee625
P-Jaswanth <pjlooser@gmail.com>
2023-10-12 22:08:58 +0530

    without local storage

--- script.js
+++ script.js
@@ -6,37 +6,132 @@ let menuBar = document.querySelector('.menu-bar');
 
 let tasks = document.querySelectorAll('.todo-list .task'); 
 
+const stateOptions = document.querySelectorAll('.option');
+
+const clearCompleted = document.querySelector('.clear');
+
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
     addTask();
-
     input.value = "";
+
 });
 
+for (let task of tasks) {
+  task.addEventListener('click', () => {
+    task.classList.toggle('done');
+    updateCount();
+  });
+};
+
+updateCount();
+
 function addTask() {
-    let task = input.value;
-    let addTask = document.createElement('div');
-    addTask.classList.add('task');
+  let task = input.value;
+  let addTask = document.createElement('div');
+  addTask.classList.add('task');
 
-    addTask.innerHTML = `
-    <div class="circle">
-        <img src="images/icon-check.svg">
-    </div>
-    <span>${task}</span>
-    <img src="images/icon-cross.svg" class="cross"> 
-    `;
+  addTask.innerHTML = `
+  <div class="circle">
+      <img src="images/icon-check.svg">
+  </div>
+  <span>${task}</span>
+  <img src="images/icon-cross.svg" class="cross"> 
+  `;
 
-    menuBar.before(addTask);
-    update();
+  menuBar.before(addTask);
+  addTask.addEventListener('click',() => {
+      addTask.classList.toggle('done');
+      updateCount();
+  });
+
+  updateCount();
+};
+
+// function update() {
+//     let tasksDone = document.querySelectorAll('done');
+//     let totalTasks = document.querySelectorAll('.todo-list .task'); 
+
+//     let itemsLeft = document.querySelector('.items-left');
+//     let count = totalTasks.length - tasksDone.length;
+//     itemsLeft.innerHTML = `
+//     <p>${count} items left</p>
+//     `;
+// }
+
+
+
+todoList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('cross')) {
+    const parentDiv = e.target.closest('.task');
+    if (parentDiv) {
+      parentDiv.remove();
+      console.log('removed');
+      updateCount();
+    }
+  }
+});
+
+function updateCount() {
+  const tasks = document.querySelectorAll('.todo-list .task');
+  const doneTasks = document.querySelectorAll('.todo-list .task.done');
+  const itemsLeft = tasks.length - doneTasks.length;
+
+  const itemsCount = document.querySelector('.items-left');
+
+  itemsCount.innerHTML = `
+  <p>${itemsLeft} items left</p>
+  `;
+
+
+};
+
+stateOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    stateOptions.forEach((opt) => {
+      opt.classList.remove('present');
+    });
+
+    option.classList.add('present');
+
+    const optionClass = option.textContent.toLowerCase();
+
+    const tasks = document.querySelectorAll('.todo-list .task');
+    tasks.forEach((task) => {
+      if (optionClass === 'all') {
+        task.style.display = '';
+        updateCount();
+      } else if (optionClass === 'active' && !task.classList.contains('done')) {
+        task.style.display = '';
+        updateCount();
+      } else if (optionClass === 'completed' && task.classList.contains('done')) {
+        task.style.display = '';
+        completedCount();
+      } else {
+        task.style.display = 'none';
+      }
+    });
+  });
+});
+
+function completedCount() {
+  const doneTasks = document.querySelectorAll('.todo-list .task.done');
+  const itemsCount = document.querySelector('.items-left');
+
+  itemsCount.innerHTML = `
+  <p>${doneTasks.length} items left</p>
+  `;
+
 }
 
-function update() {
-    tasks = document.querySelectorAll('.todo-list .task'); 
+clearCompleted.addEventListener('click', () => {
+    const clearTasks = document.querySelectorAll('.task.done');
+    clearTasks.forEach((task)=> {
+      task.remove();
+    });
 
-    for (let task of tasks) {
-        task.addEventListener('click',() => {
-            task.classList.toggle('done');
-            console.log(task);
-        });
-    };
-}
+    completedCount();
+    updateCount();
+
+
+});
